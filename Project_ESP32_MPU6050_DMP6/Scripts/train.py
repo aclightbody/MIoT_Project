dcd610b9abe1633818d2bbcda9afec5206b5dbab
Andrew Lightbody <aclightbody@googlemail.com>
2024-12-03 01:13:09 +0000

    Updated, but too slow to run on laptop

--- Project_ESP32_MPU6050_DMP6/Scripts/train.py
+++ Project_ESP32_MPU6050_DMP6/Scripts/train.py
@@ -3,14 +3,20 @@ import pandas
 import numpy
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import os
+from mlxtend.plotting import plot_decision_regions
 
 # https://stackoverflow.com/questions/55424906/building-svm-with-tensorflows-linearclassifier-and-pandas-dataframes
+# https://aiplanet.com/notebooks/2418/binthamza/svm-classification-with-tfkeras-model
+# https://stackoverflow.com/questions/55424906/building-svm-with-tensorflows-linearclassifier-and-pandas-dataframes
+# https://www.kaggle.com/code/shadesh/svm-classification-using-tensorflow-and-keras
 
 # with open("C:\\MIoT_Device\\Project\\Project_ESP32_MPU6050_DMP6_DataGather\\Scripts\\FeatureExtractDrop.csv", newline='') as file:
 #     dataDrop = list(csv.reader(file))
 
-dataDrop = pandas.read_csv("..\\Data\\FeatureExtractDrop.csv").values
-dataWalk = pandas.read_csv("..\\Data\\FeatureExtractWalk.csv").values
+cwd = os.getcwd()
+dataDrop = pandas.read_csv(cwd + "\\Data\\FeatureExtractDrop.csv").values
+dataWalk = pandas.read_csv(cwd + "\\Data\\FeatureExtractWalk.csv").values
 
 # Features
 x = dataDrop[:, 1:]
@@ -31,8 +37,6 @@ plt.ylabel("axyStdDevR")
 plt.legend(loc="upper right")
 plt.show()
 
-print()
-
 def get_model():
     #Linear SVM model
     model = tf.keras.Sequential()
@@ -49,23 +53,78 @@ def get_model():
     return model
 
 model = get_model()
+
+# batch_size: Number of samples per gradient update. If unspecified, batch_size will default to 32. The batch size is the number of training examples that you use to perform one step of stochastic gradient descent (SGD). https://stackoverflow.com/questions/61029052/what-is-the-batchsize-in-tensorflows-model-fit-function
+history = model.fit(x, y, epochs=100, verbose=False)
 model.summary()   
+print("Finished training the model")
+
+accuracy = history.history['accuracy']
+# val_accuracy = history.history['val_accuracy']
+loss = history.history['loss']
+# val_loss = history.history['val_loss']
+
+plt.figure(figsize=(5, 5))
+plt.xlabel('Epoch Number')
+plt.ylabel("Loss Magnitude")
+plt.plot(history.history['loss'])
+
+plt.figure(figsize=(5, 5))
+plt.plot(accuracy, label='Training Accuracy')
+plt.xlabel('Epoch Number')
+plt.ylabel('Accuracy')
+
+plt.figure(figsize=(5, 5))
+plt.plot(loss, label='Training Loss')
+plt.xlabel('Epoch Number')
+plt.ylabel('Loss')
+
+# plt.figure(figsize=(5, 5))
+# plt.plot(x, y) # model.layers[0].kernel.numpy(), -model.layers[0].bias.numpy()
 
 # model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-# loss, acc = model.evaluate(test_data) 
+# loss, acc = model.evaluate(test_data)
 # pred = np.argmax(model.predict(test_data), axis=1)
 # confusion = tf.math.confusion_matrix(labels=tf.constant(test_labels), predictions=tf.constant(pred), num_classes=4)
 # print(confusion)
 # print("Loss {}, Accuracy {}".format(loss, acc))
 
-# history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
-# print("Finished training the model")
-# import matplotlib.pyplot as plt
-# plt.xlabel('Epoch Number')
-# plt.ylabel("Loss Magnitude")
-# plt.plot(history.history['loss'])
-
 # history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=10, batch_size=32)
 
-# print(f' W: {model.weight}')
 # plot(X, Y, W=model.weight.t().detach().numpy(), b=-model.bias.detach().numpy())
+
+# history = model.fit(train_features, train_labels, epochs=400, batch_size=64, validation_data=(test_features, test_labels))
+
+# print(f' W: {model.weights}')
+# w0 = model.get_weights()[0] # weight vector
+# w1 = model.get_weights()[1] # bias
+
+# print(model.layers[0].kernel.numpy()) # Weight vector
+# print(-model.layers[0].bias.numpy()) # Bias
+
+# prediction = model.predict_generator(test, verbose=1)
+
+# m = - w0[0]/w0[1]
+# b = - w1[0]
+
+# xSep          = [x[1] for x in xDataset]
+# ySep          = [m * x + b for x in xSep]
+
+# plt.figure(figsize=(5, 5))
+# plt.scatter(dataDrop[:,1], dataDrop[:,2], c='red', label='Fall')
+# plt.scatter(dataWalk[:,1], dataWalk[:,2], c='blue', label='Walk')
+# plt.xlabel("axyStdDevL")
+# plt.ylabel("axyStdDevR")
+# plt.legend(loc="upper right")
+
+# plot_decision_regions(x, y, clf=model, legend=2)
+
+x_min, x_max = x[:, 0].min() - 1, x[:, 0].max() + 1
+y_min, y_max = x[:, 1].min() - 1, x[:, 1].max() + 1
+xx, yy = numpy.meshgrid(numpy.arange(x_min, x_max, 0.01),
+                         numpy.arange(y_min, y_max, 0.01))
+Z = model.predict(numpy.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+cs = plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+plt.scatter(x[:, 0], x[:, 1], c=y, cmap=plt.cm.Set1)
+plt.show()
